481385c46d7981d0d5d93ffe747451c3b01618f8
Nortti <kim93b@gmail.com>
2018-04-06 00:43:39 +0300

    Add flask hello world

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd ~/stuff
+git checkout master \
+    && git pull \
+    && rm -r venv \
+    && virtualenv -p python2 venv \
+    && . venv/bin/activate \
+    && pip install -r requirements.txt \
+    && gunicorn hello:app
