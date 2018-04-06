083ebd532f515a1e2b8d31d88588e3b39847f95b
Nortti <kim93b@gmail.com>
2018-04-06 23:26:44 +0300

    Specify production and production environments

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # stuff
 [![Build Status](https://travis-ci.org/nortti/stuff.svg?branch=master)](https://travis-ci.org/nortti/stuff)
 
+## Production
 http://nortti.win/flask
+
+## Staging
+http://nortti.win/staging/flask
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 #
-# Deploy the application from travis.
+# Deploy the application to staging or production from travis.
 
 # Exit on error
 set -e
 
-port=8200
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  port=8200
+  image=stacks-production
+elif [ "$TRAVIS_BRANCH" == "develop" ]; then
+  port=8210
+  image=stacks-staging
+fi
 
 # Build & push docker image from travis
 echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
-docker build -t "$DOCKER_USER/$DOCKER_IMAGE" \
+docker build -t "$DOCKER_USER/$image" \
   -f "$TRAVIS_BUILD_DIR"/docker/Dockerfile \
   --build-arg port="$port" \
   "$TRAVIS_BUILD_DIR"
-docker push "$DOCKER_USER/$DOCKER_IMAGE"
+docker push "$DOCKER_USER/$image"
 
 # Free travis doesn't feature ssh keys. Workaround is to store them as
 # environment variables. Instead of escaping special characters we've
@@ -21,17 +27,17 @@ docker push "$DOCKER_USER/$DOCKER_IMAGE"
 echo "$ID_RSA" | base64 --decode > id_rsa
 chmod 600 id_rsa
 
-# Pull and run instance of docker image from prod over ssh
+# Pull and run a docker container over ssh
 ssh -i id_rsa \
   -o StrictHostKeyChecking=no \
   "$PROD_USER@$PROD_ADDRESS" << EOF
     echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
-    docker pull "$DOCKER_USER/$DOCKER_IMAGE"
-    docker rm -f "$DOCKER_IMAGE"
+    docker pull "$DOCKER_USER/$image"
+    docker rm -f "$image"
     docker run -d \
       -p "$port:$port" \
-      --name "$DOCKER_IMAGE" \
-      "$DOCKER_USER/$DOCKER_IMAGE"
+      --name "$image" \
+      "$DOCKER_USER/$image"
 EOF
 
 rm id_rsa
