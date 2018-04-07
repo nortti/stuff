86cac1c5dc639b5aaeabb501ae9c991df9c2e100
Nortti <kim93b@gmail.com>
2018-04-07 15:13:54 +0300

    Tidy up test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,10 +1,8 @@
-import os
 from hello_app import hello
 import unittest
-import tempfile
 
 class HelloTestCase(unittest.TestCase):
 
     def test_hello(self):
         response = hello.app.test_client().get('/')
-        assert b'Hello world!' in response.data # This fails
+        assert b'Hello world!' in response.data
