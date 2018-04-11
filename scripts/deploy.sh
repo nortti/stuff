32b8accdb461b0f2f5c9ef6bec365654b7f27f1c
Nortti <kim93b@gmail.com>
2018-04-11 23:10:50 +0300

    Refactor deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,40 +2,41 @@
 #
 # Deploy the application to staging or production from travis.
 
-# Exit on error
-set -e
-
 if [ -z "$TRAVIS" ]; then
-    echo This script is meant to be run from travis only!
-    exit 1
+    echo This script is meant to be run from travis only! && exit 1
 fi
 
-image=stacks-$1
+image=$1
 port=$2
 
-# Build & push docker image from travis
-echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
-docker build -t "$DOCKER_USER/$image" -f Dockerfile --build-arg port="$port" .
-docker push "$DOCKER_USER/$image"
-
-# Free travis doesn't feature ssh keys. Workaround is to store them as
-# environment variables. Instead of escaping special characters we've
-# stored the key in base64-format.
-echo "$ID_RSA" | base64 --decode > id_rsa
-chmod 600 id_rsa
-
-# Pull and run a docker container over ssh
-# shellcheck disable=SC2087,SC2029
-ssh -i id_rsa \
-  -o StrictHostKeyChecking=no \
-  "$PROD_USER@$PROD_ADDRESS" << EOF
+docker_build_push() {
     echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
-    docker pull "$DOCKER_USER/$image"
-    docker rm -f "$image"
-    docker run -d \
-      -p "$port:$port" \
-      --name "$image" \
-      "$DOCKER_USER/$image"
-EOF
+    docker build -t "$DOCKER_USER/$image" -f Dockerfile --build-arg port="$port" .
+    docker push "$DOCKER_USER/$image"
+}
 
-rm id_rsa
+docker_remote_pull_deploy() {
+    # Free travis doesn't feature ssh keys. Workaround is to store them as
+    # environment variables. Instead of escaping special characters we've
+    # stored the key in base64-format.
+    echo "$ID_RSA" | base64 --decode > id_rsa
+    chmod 600 id_rsa
+
+    # Pull and run a docker container over ssh
+    # shellcheck disable=SC2087,SC2029
+    ssh -i id_rsa \
+      -o StrictHostKeyChecking=no \
+      "$PROD_USER@$PROD_ADDRESS" << EOF
+        echo "$DOCKER_PASS" | docker login -u="$DOCKER_USER" --password-stdin
+        docker pull "$DOCKER_USER/$image"
+        docker rm -f "$image"
+        docker run -d \
+          -p "$port:$port" \
+          --name "$image" \
+          "$DOCKER_USER/$image"
+EOF
+    rm id_rsa
+}
+
+set -e # Terminate on error
+docker_build_push && docker_remote_pull_deploy
