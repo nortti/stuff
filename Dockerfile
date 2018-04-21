25d3ad059cf91c3644446a103e933b2c8038cbc4
Nortti <kim93b@gmail.com>
2018-04-21 12:43:59 +0300

    Hardcode port used inside docker as it's irrelevant

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM python:2.7-slim
-ARG port
 
 COPY . .
 
 RUN scripts/install.sh prod
 
-ENTRYPOINT ["scripts/docker_entrypoint.sh", "${port}"]
-EXPOSE "${port}"
+ENTRYPOINT ["scripts/docker_entrypoint.sh"]
+EXPOSE "80"
--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -4,12 +4,10 @@
 
 set -e # Exit on error
 
-port="$1"
-
 # cd to project root
 cd "$(dirname "$0")/.."
 
 #shellcheck disable=SC1091
 source venv/bin/activate
 
-gunicorn --bind 0.0.0.0:"${port}" hello_app.hello:app
+gunicorn --bind 0.0.0.0:80 hello_app.hello:app
